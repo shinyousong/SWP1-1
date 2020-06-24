4444720af888b186e45ecf726bc5c64dd7fcbcce
shinyousong <onlyforresttime@gmail.com>
2020-06-24 13:27:56 +0900

    issue #5, first version of printing sum and mul

---
+++ calc/template.py
@@ -0,0 +1,15 @@
+html = """
+<html>
+	<body>
+		<form action="">
+			first number=<input type="number" name="num1"><br>
+			second number=<input type="number" name="num2"><br>
+			<input type="submit">
+		</form>
+		sum=%(sum)d<br>
+		mul=%(mul)d
+	</body>
+</html>
+"""
+
+		
